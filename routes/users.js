c30ea8442fe6c252c2fd3b80ac0c2b00f7f8ae51
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2019-02-11 13:35:08 +0100

    Add regex to route for user ID

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 const UserController = require('../controllers/UserController');
 
 router.route('/').get(UserController.index).post(UserController.create);
+router
+	.route('/:id([0-9a-fA-F]{24})')
+	.get(UserController.show)
+	.put(UserController.update)
+	.delete(UserController.destroy);
 router.route('/search').get(UserController.search);
-router.route('/:id').get(UserController.show).put(UserController.update).delete(UserController.destroy);
 
 module.exports = router;
